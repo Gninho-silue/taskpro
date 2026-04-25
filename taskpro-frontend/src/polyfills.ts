71cb078de6c5fabde5e0d6d46c0537b73033e64e
Gninninmaguignon Silue <gninhosilue@gmail.com>
2026-04-25 16:47:08 +0100

    Delete frontend folder

--- taskpro-frontend/src/polyfills.ts
+++
@@ -1,8 +0,0 @@
-// Basic polyfills for libraries expecting Node globals in browser
-// Ensure this loads before any other imports that may rely on it
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-(window as any).global = window;
-// Optional: minimal process shim if some libs read process.env
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-(window as any).process = (window as any).process || { env: {} };
-
